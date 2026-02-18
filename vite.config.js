e18d5d954002dc18002cdd7fffe00c5a34b1f41d
timfu1212 <timfu30915@gmail.com>
2026-02-18 19:23:40 +0800

    del: comment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Mini-games-hub/", // ✅ 必須跟 repository 名稱一樣
+  base: "/Mini-games-hub/", 
 });
